f8b825eb2e248b3dae09329d431032ea580096ce
Yarovoy <lambda2712@gmail.com>
2026-04-14 08:21:42 +0300

    1-4 increments

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/Yandex-Practicum/go-musthave-metrics-tpl/internal/server"
+	"github.com/Yandex-Practicum/go-musthave-metrics-tpl/internal/storage"
+)
+
+func main() {
+	addr := flag.String("a", "localhost:8080", "HTTP server listen address")
+	flag.Parse()
+
+	store := storage.NewMemStorage()
+	mux := server.NewRouter(store)
+
+	if err := http.ListenAndServe(*addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
